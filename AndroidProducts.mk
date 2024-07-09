b640cedd4bc3b0dd8c570cbba1534a79447c9bc2
Ni20110118 <153351536+Ni20110118@users.noreply.github.com>
2024-07-09 15:57:12 +0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_sl8541e_1h10.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_sl8541e_1h10-user \
+    omni_sl8541e_1h10-userdebug \
+    omni_sl8541e_1h10-eng
