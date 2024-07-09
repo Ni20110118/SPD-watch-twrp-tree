a0dea83d6d0aeb0a301c5c5e6b0f63cec134e9f9
Ni20110118 <153351536+Ni20110118@users.noreply.github.com>
2024-07-09 23:58:23 +0800

    更新 AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_CD10.mk
+    $(LOCAL_DIR)/omni_eb12_v30_bdz_s18_00_cn_zx_435_256x16b.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_CD10-user \
-    omni_CD10-userdebug \
-    omni_CD10-eng \
+    omni_eb12_v30_bdz_s18_00_cn_zx_435_256x16b-user \
+    omni_eb12_v30_bdz_s18_00_cn_zx_435_256x16b-userdebug \
+    omni_eb12_v30_bdz_s18_00_cn_zx_435_256x16b-eng \
