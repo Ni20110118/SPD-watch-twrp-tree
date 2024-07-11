f4193b1f03d47c738b26cacea35ddd4516645ee6
Ni20110118 <153351536+Ni20110118@users.noreply.github.com>
2024-07-11 12:55:32 +0800

    Update omni_YUHO_Y1_StarS.mk

--- omni_YUHO_Y1_StarS.mk
+++ omni_YUHO_Y1_StarS.mk
@@ -10,7 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from YUHO_Y1_StarS device
 $(call inherit-product, device/yuho/YUHO_Y1_StarS/device.mk)
